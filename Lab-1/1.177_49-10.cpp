08088d292c884b2964bccbb720d83603f0e22320
Mohaiminur-177 <109685451+Mohaiminur-177@users.noreply.github.com>
2022-07-26 14:52:30 +0600

    Add files via upload

---
+++ Lab-1/1.177_49-10.cpp
@@ -0,0 +1,18 @@
+//Codeforce Problem No- 4A
+//Problem Name- Watermelon
+
+#include<iostream>
+using namespace std;
+
+int main(){
+int w;
+cin >> w;
+if(w%2==0&&w!=2){
+    cout<<"YES";
+}
+else{
+    cout<<"NO";
+}
+
+return 0;
+}
