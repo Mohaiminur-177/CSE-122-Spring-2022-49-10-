2622f57dc6c5f67955bbb101604275e43312f91a
Mohaiminur Rahman <109685451+Mohaiminur-177@users.noreply.github.com>
2022-08-09 20:17:31 +0600

    Add files via upload

---
+++ Lab-4/2.177_49-10.cpp
@@ -0,0 +1,14 @@
+// Codeforce Problem No - 281A
+// Problem Name - Word Capitalization
+
+
+#include<iostream>
+using namespace std;
+int main(){
+string s;
+cin>>s;
+s[0]=towupper(s[0]);
+
+cout<<s;
+
+}
